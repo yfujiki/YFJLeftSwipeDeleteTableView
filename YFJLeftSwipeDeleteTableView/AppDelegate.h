3feff7f4f435723d7edf2fe9267e20739076d8e0
Yuichi Fujiki <yuichi@chaione.com>
2013-06-27 12:48:51 -0500

    Initial Commit

---
+++ YFJLeftSwipeDeleteTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YFJLeftSwipeDeleteTableView
+//
+//  Created by Yuichi Fujiki on 6/27/13.
+//  Copyright (c) 2013 Yuichi Fujiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YFJLeftSwipeDeleteTableViewTests/YFJLeftSwipeDeleteTableViewTests.m
@@ -0,0 +1,36 @@
+//
+//  YFJLeftSwipeDeleteTableViewTests.m
+//  YFJLeftSwipeDeleteTableViewTests
+//
+//  Created by Yuichi Fujiki on 6/27/13.
+//  Copyright (c) 2013 Yuichi Fujiki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YFJLeftSwipeDeleteTableViewTests : XCTestCase
+
+@end
+
+@implementation YFJLeftSwipeDeleteTableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
